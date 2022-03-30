6791ec46d7e1297a26acb41c6cef84e5fdaa4e39
Proffybean <jeff.balsley@gmail.com>
2022-03-29 20:28:42 -0700

    init commit

---
+++ SuperNinja.js
@@ -0,0 +1,48 @@
+// import Ninja from './Ninja'
+
+class Ninja{
+    constructor(name, health){
+        this.name = name
+        this.health = health
+        this.speed = 3
+        this.strength = 3
+    }
+
+    sayName(){
+        console.log(`My name is ${this.name}`)
+    }
+
+    showStats(){
+        console.log(`name: ${this.name}`)
+        console.log(`health: ${this.health}`)
+        console.log(`speed: ${this.speed}`)
+        console.log(`strength: ${this.strength}`)
+    }
+
+    drinkSake(){
+        this.health += 10;
+    }
+}
+
+
+class Sensei extends Ninja {
+    constructor(name ){
+        super(name, 200)
+        this.wisdom = 10
+        
+        this.speed = 10
+        this.strength = 10
+    }
+
+    speakWisdom(){
+        super.drinkSake()
+        console.log('What one programmer can do in one month, two programmers can do in two months')
+    }
+}
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+
+superSensei.showStats();
+
